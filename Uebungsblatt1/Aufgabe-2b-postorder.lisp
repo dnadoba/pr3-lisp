ea9d8a6a753f795c6ae791437b7638963b13c825
SeJV <sebastian1210@web.de>
2017-11-25 11:58:11 +0100

    2b-postorder

---
+++ Uebungsblatt1/Aufgabe-2b-postorder.lisp
@@ -0,0 +1,18 @@
+(setf example (list 5 (list 3 (list 2 (list 1 () ()) ()) (list 4 () ())) (list 8 (list 6 () (list 7 () ())) (list 9 () (list 10 () ())))))
+(print example)
+
+(defun postorder(binaryTree)
+  (append 
+    (if (null (first(cadr binaryTree)))
+        ()
+        (postorder(cadr binaryTree))
+    )   
+    (if (null (first(last binaryTree)))
+        ()
+        (postorder(first(last binaryTree)))
+    ) 
+   (list (first binaryTree))
+   )
+)
+
+(print (postorder example))
