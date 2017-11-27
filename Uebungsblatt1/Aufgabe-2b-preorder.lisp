c76d5725da164f7044eeb1f0f8b8d5ad11c50c90
SeJV <sebastian1210@web.de>
2017-11-27 09:42:14 +0100

    print

--- Uebungsblatt1/Aufgabe-2b-preorder.lisp
+++ Uebungsblatt1/Aufgabe-2b-preorder.lisp
@@ -1,8 +1,7 @@
 (setf example (list 5 (list 3 (list 2 (list 1 () ()) ()) (list 4 () ())) (list 8 (list 6 () (list 7 () ())) (list 9 () (list 10 () ())))))
 
 (defun postorder(binaryTree)
-  (append 
-    (list (first binaryTree))
+    (print (first binaryTree))
     (if (null (first(cadr binaryTree)))
         ()
         (postorder(cadr binaryTree))
@@ -11,7 +10,6 @@
         ()
         (postorder(first(last binaryTree)))
     ) 
-   )
 )
 
 (postorder example)
