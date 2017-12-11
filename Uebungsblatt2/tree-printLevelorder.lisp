1a909858a93351aad9379e7c989dfd9260dfea51
David Nadoba <dnadoba@gmail.com>
2017-12-11 18:32:31 +0100

    printLevelorder

---
+++ Uebungsblatt2/tree-printLevelorder.lisp
@@ -0,0 +1,20 @@
+(load "tree-height.lisp")
+
+(defun node-printLevelorderR (node crtLevel printLevel)
+	(if (eq crtLevel printLevel)
+		(if (first node) (print (first node)))
+		(progn 
+			(node-printLevelorderR (second node) (+ 1 crtLevel) printLevel)
+			(node-printLevelorderR (third node) (+ 1 crtLevel) printLevel)
+			)
+		)
+	)
+
+
+(defun tree-printLevelOrder (node)
+		(loop for printLevel from 0 to (tree-height node) do
+				(node-printLevelorderR node 0 printLevel)
+			)
+	)
+
+(tree-printLevelOrder `(2 (1) (3)))
