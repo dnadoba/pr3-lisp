d3030af3d6173a579d9a05109b03e4fb07a0ca26
SeJV <sebastian1210@web.de>
2017-12-13 20:48:32 +0100

    ...

--- Uebungsblatt2/tree-insert.lisp
+++ Uebungsblatt2/tree-insert.lisp
@@ -1,16 +1,9 @@
 (defun insert(baum val)
     (cond
-		((null (first baum))
-			(setq baum(list val nil nil))
-		)
-        ((> (first baum) val) 
-             (setq baum (list (first baum) (insert (second baum) val) (third baum)))
-        )
-        ((< (first baum) val) 
-             (setq baum (list (first baum) (second baum) (insert (third baum) val)))
-		)
+		((null (first baum))(list val nil nil))
+        ((> (first baum) val)(list (first baum) (insert (second baum) val) (third baum)))
+        ((< (first baum) val)(list (first baum) (second baum) (insert (third baum) val)))
 	)
-	baum
 )  
 
 (defun readFile (filename)
@@ -22,21 +15,29 @@
     )
 )
 
+(defun addfile (tree fileName)
+    (setq tree (insertAll tree (readFile fileName)))
+	tree
+)
+
+(defun insertAll(tree values)
+	(cond 
+		((not (eql (first values) nil))
+			(setq tree (insert tree (first values)))
+			(setq tree (insertAll tree (rest values)))
+		)
+	)
+	tree
+)
+
 (defun addAll(tree otherTree)
-	(cond((first otherTree)
-       (setq tree (insert tree (first otherTree)))
-	   
-	   (if(second otherTree)
-			(setq tree (addAll tree (second otherTree)))
-	   )
-		(if(third otherTree)
-			(setq tree (addAll tree (third otherTree)))
-	   )	   
-	   )
+	(cond 
+		((not(eql(first otherTree)nil))
+			(setq tree (insert tree (first otherTree)))
+			(if(second otherTree)(setq tree (addAll tree (second otherTree))))
+			(if(third otherTree)(setq tree (addAll tree (third otherTree))))	   
+		)
 	)
 	tree
 )    
 
-(defun addFile(tree fileName)
-    (setq tree(addAll tree (readFile fileName)))
-)
